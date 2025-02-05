d76b4f850ffdbc583f3419be3ab0fb1b9da0de77
rahulsharma9001 <rahul.sharmaa9001@gmail.com>
2025-02-06 01:56:21 +0530

    Integrated mySQL(AIVEN)  with Spring

---
+++ src/main/java/com/example/Spring_mySQL/MainController.java
@@ -0,0 +1,27 @@
+package com.example.Spring_mySQL;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+
+@RequestMapping(path = "/Spring-mySQL")
+public class MainController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostMapping(path = "/add")
+    public @ResponseBody User addNewUser(@RequestParam String name ,@RequestParam String email){
+        User springUser = new User();
+        springUser.setName(name);
+        springUser.setEmail(email);
+        userRepository.save(springUser);
+        return springUser;
+    }
+
+    @GetMapping(path = "/all")
+    public @ResponseBody Iterable<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+}
---
+++ src/test/java/com/example/Spring_mySQL/SpringMySqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_mySQL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMySqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
